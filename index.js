a883a7ca0b14b763422191d261c163ceb83964db
Diego-Pardo45 <diegopardopersonal@gmail.com>
2025-09-01 14:39:22 -0400

    Learning about http petitions
    practicing Post and Get methods again but with an array of elements

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+
 const PORT = 3000;
 
+const students_list = [{"name": "Diego"}];
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('The list of students is: ' + JSON.stringify(students_list));
 });
 
 app.get('/other', (req, res) => {
@@ -12,7 +18,9 @@ app.get('/other', (req, res) => {
 });
 
 app.post('/name', (req, res) => {
-    res.send('POST request to the name route');
+    const body = req.body;
+    students_list.push(body);
+    res.send('POST request to the "name" route');
 });
 
 app.listen(PORT, () => {
